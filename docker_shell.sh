55ea84f096f72647b0152b7c117b6a83191a1f31
Louis Rubet <louis@rubet.fr>
2021-02-17 09:03:20 +0100

    Parametrize yocto path

--- docker_shell.sh
+++ docker_shell.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+#YOCTO_PATH=/home/louis/Development/ws-yocto-zeus
+YOCTO_PATH=/home/louis/Development/ws-yocto
+
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 export LANGUAGE=en_US.UTF-8
 
-export TEMPLATECONF=/home/louis/Development/ws-yocto/conf
-cd /home/louis/Development/ws-yocto && source /home/louis/Development/ws-yocto/poky/oe-init-build-env build && /usr/bin/zsh
+export TEMPLATECONF=${YOCTO_PATH}/conf
+cd ${YOCTO_PATH} && source ${YOCTO_PATH}/poky/oe-init-build-env build && /usr/bin/zsh
