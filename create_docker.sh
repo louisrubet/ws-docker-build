ea8621bbcbdc174d1a27c74bc0b5dc40e8824847
Louis Rubet <louis@rubet.fr>
2021-06-04 09:12:00 +0200

    pass user as an argument

--- create_docker.sh
+++ create_docker.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-docker build -t yocto_builder .
+docker build -t yocto_builder -f Dockerfile_16-04 \
+  --build-arg USER_ID=$(id -u) \
+  --build-arg GROUP_ID=$(id -g) .
--- run_docker.sh
+++ run_docker.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 docker run \
-	--privileged \
 	-v /var/yocto-cache:/var/yocto-cache \
-	-v /home/louis:/home/louis \
-	-u louis:louis \
-	-e DISPLAY \
+	-v /home/louis:/home/user \
+	-u user:user \
 	-i -t yocto_builder
